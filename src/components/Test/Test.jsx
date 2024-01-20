712c0a5f86e0154477739521cb58a66c00019664
RandyCheBro <kolyavolya@yandex.ru>
2024-01-20 07:37:37 +0300

    добавляет компонент BackLink

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import BackLink from "../BackLink/BackLink";
+
 import styles from "./Test.module.scss";
 
 function Test() {
   return (
     <div className={styles.test}>
+      <BackLink 
+      text="Назад" 
+      link="/" 
+      />
       <h1>Test Test Test Test Test Test Test</h1>
     </div>
   );
