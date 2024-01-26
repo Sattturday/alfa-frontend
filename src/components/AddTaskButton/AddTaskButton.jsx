dd8934e5157ce69a7ce83c22ac7cbd1dc85886d9
Dmitry Rusinov <kachek96@yandex.ru>
2024-01-26 14:54:17 +0300

    create CreateIPR page, components IPRCard, AddTaskButton

---
+++ src/components/AddTaskButton/AddTaskButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./AddTaskButton.module.scss";
+
+function AddTaskButton() {
+  return <button type="button" className={styles.button} disabled>
+  </button>;
+}
+
+export default AddTaskButton;
