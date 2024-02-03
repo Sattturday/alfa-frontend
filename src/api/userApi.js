19106824f11c9064ef763a5608be1924451e9c9a
Alla Magina <babara@flylady.su>
2024-02-03 22:37:37 +0300

    fix: correct Api

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,36 +1,31 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { BASE_URL } from '../utils/constants';
-//import { getAuthToken } from '../utils/utils';
+import { getAuthToken } from '../utils/utils';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
-    // если нужно будет, чтобы токен добавлялся во все запросы, то можно сюда добавить заготовку из authApi
+    prepareHeaders: (headers, { getState }) => {
+      const token = getAuthToken();
+
+      if (token) {
+        headers.set('authorization', `Token ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: (build) => ({
-    // Заготовка запроса для списка всех ИПР с токеном в заголовке
-    // getAllIDP: build.query({
-    //   query: () => {
-    //     const token = getAuthToken();
-    //     const authHeader = token ? { Authorization: `Bearer ${token}` } : {};
-
-    //     return {
-    //       url: 'employee/get_all_users_and_ipr/',
-    //       headers: authHeader,
-    //     };
-    //   },
-    // }),
-
-    // Определение запросa для получения списка всех ИПР
-    getAllIDP: build.query({
-      query: () => 'employee/get_all_users_and_ipr/',
+    // Определение запросa для получения данных авторизованного пользователя
+    getMe: build.query({
+      query: () => 'employee/me',
     }),
-    getUserIDP: build.query({
-      query: () => 'employee_get_ipr/',
+    // Определение запросa для получения списка всех подчиненных с ИПР
+    getAllIDP: build.query({
+      query: () => 'employee/my_employee/',
     }),
   }),
 });
 
-export const { useGetAllIDPQuery, useGetUserIDPQuery } = userApi;
+export const { useGetMeQuery, useGetAllIDPQuery } = userApi;
