275e3c4f099986320feee3b200ed969634bd3529
Alla Magina <babara@flylady.su>
2024-02-04 17:01:52 +0300

    fix: correct Login

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -19,7 +19,7 @@ export const userApi = createApi({
   endpoints: (build) => ({
     // Определение запросa для получения данных авторизованного пользователя
     getMe: build.query({
-      query: () => 'employee/me',
+      query: () => 'employee/me/',
     }),
     // Определение запросa для получения списка всех подчиненных с ИПР
     getAllIDP: build.query({
